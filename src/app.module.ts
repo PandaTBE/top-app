5e2f8e5bea4ee259f1e05080dae123603edb247f
Nikita Surov <surov.n2012@gmail.com>
2022-07-28 19:29:08 +0300

    добавил env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 
 @Module({
-    imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [ConfigModule.forRoot(), AuthModule, TopPageModule, ProductModule, ReviewModule],
 })
 export class AppModule {}
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+    constructor(private readonly configService: ConfigService) {}
+
     /** Omit позволяет исключить из класса переданные поля */
     @Post('create')
-    async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+    async create(@Body() dto: Omit<TopPageModel, '_id'>) {
+        this.configService.get('TEST');
+    }
 
     @Get(':id')
     async get(@Param('id') id: string) {}
